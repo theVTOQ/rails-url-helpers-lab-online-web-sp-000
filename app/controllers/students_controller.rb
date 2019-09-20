eec9654af4e4ba6069d9b762baeb138cb4ce1b1b
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-20 02:55:49 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,16 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate_student_path
+    @student = set_student
+    render "activate"
   end
 
   private
