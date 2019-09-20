2efdd92014fb4472b3f67ec78959ffa4fbc5d6b7
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-20 03:43:35 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def toggle_activate
+    @student = set_student
+    new_state = !@student.active
+    @student.update(active: new_state)
+    redirect_to @student
   end
 
   private
